6baf0cee468e0e21e129eb073f6d84716a35363a
lhbbbb <lhb4196@gmail.com>
2020-01-31 09:49:57 +0900

    branch test

--- frontend/src/Components/Routers/Main.tsx
+++ frontend/src/Components/Routers/Main.tsx
@@ -11,9 +11,7 @@ import Hamburger from "../Common/Hamburger";
 import Ganji from "../Main/Ganji";
 import { makeStyles } from "@material-ui/core/styles";
 
-
 function Main() {
-
   const useStyles = makeStyles(theme => ({
     root: {
       display: "flex",
@@ -29,24 +27,24 @@ function Main() {
       <Header></Header>
       <Nav></Nav>
       <div className={classes.root}>
-      <div className="Main">
-        <Ganji></Ganji>
-        <div className="famous">
-          <h4 className="famous_text">두근 마켓 인기 매물</h4>
-          <div className="famous_bundle">
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
-          </div>
-          <div className="famous_bundle">
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
-            <div className="div_famous"></div>
+        <div className="Main">
+          <Ganji></Ganji>
+          <div className="famous">
+            <h4 className="famous_text">두근 마켓 인기 매물</h4>
+            <div className="famous_bundle">
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+            </div>
+            <div className="famous_bundle">
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+              <div className="div_famous"></div>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <Hamburger></Hamburger>
       <PenButton></PenButton>
