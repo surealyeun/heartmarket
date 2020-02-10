3e64ce020307b11f72ab610ca0c7f2294b019598
multicampus <ssongda9412@naver.com>
2020-02-10 16:18:14 +0900

    S02P13A208 글쓰기 기능 추가 중

--- frontend/src/Components/Routers/Write.tsx
+++ frontend/src/Components/Routers/Write.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 // import 'antd/dist/antd.css';
 //npm install antd --save
 //https://gary-shen.github.io/ant-design/components/upload/
- 
+
 import Header from "../Common/Header";
 import Nav from "../Common/Nav";
 import TopButton from "../Common/TopButton";
@@ -17,8 +17,8 @@ class Write extends Component {
   state = {
     title: "",
     explain: "",
-    price:"",
-    category: 1,
+    price: "",
+    category: "1",
     images: [],
     filekey: 0
   };
@@ -46,7 +46,7 @@ class Write extends Component {
           this.setStateAsync({
             images: e.target.files
           });
-          console.log(this.state.images);
+           console.log(this.state.images);
         }
       }
     }
@@ -54,55 +54,70 @@ class Write extends Component {
 
   handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     
-        axios({
-            method: "post",
-            url: "http://13.125.55.96:8080/trade/add",
-            params: {
-              user: this.user.id,
-              title: this.state.title,
-              price: this.state.price,
-              explain: this.state.explain,
-              category: this.state.category,
-              images: this.state.images
-            }
-        })
-            .then(res => {
-                console.log(res.data.data);
-            })
-            .catch(error => {
-                console.log(error);
-                console.log(this.state);
-                alert("아이디와 비밀번호를 확인해주세요.");
-            });
-        e.preventDefault();
+    //const formData = new FormData();
+    console.log(this.state.images);
+    // this.state.images.forEach(file => {
+    //   formData.append('files', file);
+    // })
 
-};
+    // console.log(formData.values);
+    e.preventDefault();
+
+    axios({
+      method: "post",
+      url: "http://13.125.55.96:8080/trade/add",
+      headers: {
+        "content-type": "multipart/form-data"
+      },
+      data: {
+        userNo: this.user.id,
+        tradeArea: this.user.address,
+        tradeTitle: this.state.title,
+        productPrice: this.state.price.toString,
+        productInfo: this.state.explain,
+        tradeCategory: this.state.category,
+        productName: this.state.title,
+        //files : formData
+      }
+    })
+      .then(res => {
+        console.log(res.data.data);
+        console.log("tjdddd");
+      })
+      .catch(error => {
+        //console.log(error);
+        //console.log(this.state);
+        alert(error);
+        e.preventDefault();
+      });
+      e.preventDefault();
+  };
 
   render() {
     return (
       <>
+        <Header></Header>
+        <Nav></Nav>
         <div className="Write">
-          <Header></Header>
-          <Nav></Nav>
           <form onSubmit={this.handleSubmit}>
             <div>
               <input
-                className="write_input half"
+                className="write_address"
                 value={this.user.uarea[0].address}
                 type="text"
                 readOnly
               ></input>
               <select
-                className="write_input half2"
+                className="write_category"
                 name="category"
                 onChange={e => this.InputChange(e)}
               >
-                <option value={1}>카테고리1</option>
-                <option value={2}>카테고리2</option>
-                <option>카테고리3</option>
-                <option>카테고리4</option>
-                <option>카테고리5</option>
-                <option>카테고리6</option>
+                <option value={"1"}>카테고리1</option>
+                <option value={"2"}>카테고리2</option>
+                <option value={"3"}>카테고리3</option>
+                <option value={"4"}>카테고리4</option>
+                <option value={"5"}>카테고리5</option>
+                <option value={"6"}>카테고리6</option>
               </select>
               <input
                 className="write_input"
@@ -116,43 +131,51 @@ class Write extends Component {
                 className="write_input"
                 name="price"
                 type="number"
+                min="0"
                 placeholder="가격을 입력해주세요."
                 onChange={e => this.InputChange(e)}
                 required
               ></input>
               <textarea
-                className="write_input textarea"
+                className="textarea"
                 name="explain"
                 placeholder="상품 설명을 입력해주세요"
                 onChange={e => this.InputChange(e)}
                 required
               ></textarea>
               <div className="filebox">
-                <label>제품 사진 업로드
-                <input
-                  key={this.state.filekey}
-                  className="write_input"
-                  multiple
-                  id="ex_file"
-                  type="file"
-                  name="images"
-                  onChange={e => this.InputChange(e)}
-                  required
-                />
+                <label>
+                  제품 사진 업로드
+                  <input
+                    key={this.state.filekey}
+                    className="write_input"
+                    multiple
+                    id="ex_file"
+                    type="file"
+                    name="images"
+                    onChange={e => this.InputChange(e)}
+                    required
+                  />
                 </label>
-                {this.state.images.length}개의 사진이 업로드 되었습니다.
+                <div className="write_filenum">
+                  {this.state.images.length}개의 사진이 업로드 되었습니다.
+                </div>
               </div>
-              {this.state.images.length === 0? "" : (<div>
-              <button>등록</button>
-              <button>취소</button>
-              </div>)}
-              
+              {this.state.images.length === 0 ? (
+                ""
+              ) : (
+                <div>
+                  <button type="submit">등록</button>
+                  <button>취소</button>
+                </div>
+              )}
+
               <button>등록</button>
               <button>취소</button>
             </div>
           </form>
-          <TopButton></TopButton>
         </div>
+        <TopButton></TopButton>
         <Footer></Footer>
       </>
     );
