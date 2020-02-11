1d063fe0f03f21e1476d64142b53a1e895400382
surealyeun <monkyw@naver.com>
2020-02-11 16:36:14 +0900

    0211 after merge make new branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 // import Nav from './Components/Common/Nav'
 // import Button from './Components/Common/Button'
 // import Footer from './Components/Common/Footer'
-import Main from './components/routers/Main'
-import Write from './components/routers/Write'
-import Alarm from './components/routers/Alarm'
-import DetailAlarm from './components/routers/DetailAlarm'
+import Main from './Components/routers/Main'
+import Write from './Components/routers/Write'
+import Alarm from './Components/routers/Alarm'
+import DetailAlarm from './Components/routers/DetailAlarm'
 
-import SearchResult from "./components/routers/SearchResult"
+import SearchResult from "./Components/routers/SearchResult"
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 
 
-import Login from './components/routers/Login';
-import Join from './components/routers/Join';
-import Mypage from './components/routers/Mypage';
-import JoinDetail from './components/routers/JoinDetail';
-import JoinSuccess from './components/routers/JoinSuccess'; 
-import UpdateUser from './components/routers/UpdateUser';
-import Sale from './components/routers/SaleMore';
-import Purchase from './components/routers/PurchaseMore';
-import UserProfile from './components/routers/UserProfile';
-import Detail from './components/routers/Detail';
+import Login from './Components/routers/Login';
+import Join from './Components/routers/Join';
+import Mypage from './Components/routers/Mypage';
+import JoinDetail from './Components/routers/JoinDetail';
+import JoinSuccess from './Components/routers/JoinSuccess'; 
+import UpdateUser from './Components/routers/UpdateUser';
+import Sale from './Components/routers/SaleMore';
+import Purchase from './Components/routers/PurchaseMore';
+import UserProfile from './Components/routers/UserProfile';
+import Detail from './Components/routers/Detail';
 
 function App() {
   return (
